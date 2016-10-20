af109455d7e73fc661706dbc46e2686a288aa074
Study-Zhou <mengqidluffy@gmail.com>
2016-10-20 16:56:12 +0800

    finish to play wav audio data

---
+++ app/src/main/java/fruitbasket/com/audioprocessor/record/WavRecordTask.java
@@ -0,0 +1,14 @@
+package fruitbasket.com.audioprocessor.record;
+
+/**
+ * 用于录制wav格式的音频
+ * Created by Study on 20/10/2016.
+ */
+
+public class WavRecordTask implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
