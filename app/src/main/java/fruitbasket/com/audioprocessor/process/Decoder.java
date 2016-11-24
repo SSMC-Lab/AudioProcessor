2baf8f1be41b702837da1456d079eb61ac7ca778
Fruit-Basket <mengqidluffy@gmail.com>
2016-11-24 09:08:38 +0800

    improve the result in another way

--- app/src/main/java/fruitbasket/com/audioprocessor/process/Decoder.java
+++ app/src/main/java/fruitbasket/com/audioprocessor/process/Decoder.java
@@ -3,9 +3,10 @@ package fruitbasket.com.audioprocessor.process;
 import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.ListIterator;
-import java.util.Queue;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 
 import fruitbasket.com.audioprocessor.process.fft.STFT;
@@ -21,7 +22,7 @@ final class Decoder {
     /*
     指定声音检测的最大次数
      */
-    private static final int MAX_DECTECTION=50;
+    private static final int MAX_DECTECTION=70;
 
     private STFT stft;
 
@@ -29,14 +30,15 @@ final class Decoder {
     private ArrayBlockingQueue<short[]> audioDataBuffer;
 
     private ArrayList<Integer> temIndexs;//存放当前的声音信息识别结果，以在PCondition.WAVE_RATE_BOOK对应的元素位置表示
-    private int[][] decodeIndexs;//存放最优的声音信息识别结果，以在PCondition.WAVE_RATE_BOOK对应的元素位置表示
+    private int[][] decodeIndexs;//存放最优的一些声音信息识别结果，以在PCondition.WAVE_RATE_BOOK对应的元素位置表示
+                                                    //按照由好到坏排序。好的放在前面，坏的放在后面
     private String decodeString;//存放最优的声音信息识别结果，以字符表示。
 
 
     public Decoder(){
         stft=new STFT(STFT.FFT_LENGTH_8192);
         audioDataBuffer=new ArrayBlockingQueue<short[]>(MAX_DECTECTION);
-        decodeIndexs=new int[3][];
+        decodeIndexs=new int[5][];
         temIndexs=new ArrayList<>();
     }
 
@@ -81,7 +83,7 @@ final class Decoder {
                                 break;
                             }
                         }
-                        merge(toArray(temIndexs));
+                        storeBestResult(toArray(temIndexs));
                         temIndexs.clear();
                     }
                 } else if (index == PCondition.START_INDEX) {
@@ -101,7 +103,7 @@ final class Decoder {
                             }
                         }
 
-                        merge(toArray(temIndexs));
+                        storeBestResult(toArray(temIndexs));
                         temIndexs.clear();
                     }
                 } else {
@@ -112,13 +114,13 @@ final class Decoder {
             ++detectionCounter;
         }
         //进行最后的信息合并
-        merge(toArray(temIndexs));
+        storeBestResult(toArray(temIndexs));
 
         /*
         结束上述处理后的结果：
         1.decodeIndexs得到正确的结果，temIndex有可能为空也有可能不空
-        2.decodeIndexs==null，但是temIndex！=null
-        3.temIndexs==null&&decodeIndexs==null
+        2.decodeIndexs[0]==null，但是temIndex！=null
+        3.temIndexs==null&&decodeIndexs[0]==null
          */
         //分情况处理结果
         if(temIndexs==null&&decodeIndexs==null){
@@ -131,12 +133,19 @@ final class Decoder {
         }
 
         temIndexs.clear();
-        decodeString = stringOfIndexs(decodeIndexs[0]);
+        //decodeString = stringOfIndexs(decodeIndexs[0]);
+        decodeString=getDecodeString();
         Log.i(TAG, "decodeString=" + decodeString);
         return decodeString;
     }
 
-    private void merge(int[] array){
+    /**
+     * 存放最优的声音识别结果
+     * 这里认为，识别的结果越长，就越优
+     * @param array
+     */
+    private void storeBestResult(int[] array){
+        Log.i(TAG,"storeBestResult()");
         if(array==null){
             return;
         }
@@ -150,12 +159,121 @@ final class Decoder {
                 int j=decodeIndexs.length-1;
                 while(j>i){
                     decodeIndexs[j]=decodeIndexs[j-1];
+                    j--;
                 }
                 decodeIndexs[i]=array;
                 break;
             }
             ++i;
         }
+    }
+
+    /**
+     * 根据最优的一些声音设备结果，取得最优的识别结果，并以字符串表示。
+     * 此函数有待改进
+     */
+    private String getDecodeString(){
+        Log.i(TAG,"getDecodeString()");
+        for(int i=0;i<decodeIndexs.length;++i){
+            Log.i(TAG,stringOfIndexs(decodeIndexs[i]));
+        }
+        ArrayList<Integer> bestResult=new ArrayList<>();
+
+        int[] ptArray=new int[decodeIndexs.length];
+        int [] sameCounter=new int[decodeIndexs.length];
+        int[] sameNumberCounter=new int[decodeIndexs.length];
+
+        for(int i=0;i<ptArray.length;++i){
+            ptArray[i]=0;
+        }
+
+        //
+        int maxElement=0;
+        int maxElementIndex=0;
+        int[] elements=new int[decodeIndexs[0].length];
+        int pt=0;
+        int i,j,k;
+        for(i=0;i<decodeIndexs.length;++i){
+            if(decodeIndexs[i]!=null){
+                pt=0;
+                elements[pt]=decodeIndexs[i][0];
+                for(j=1;j<decodeIndexs[i].length;++j){
+
+                    for(k=0;k<=pt;++k){
+                        if(decodeIndexs[i][j]==elements[k]) {
+                            break;
+                        }
+                    }
+                    if(k>pt){
+                        elements[++pt]=decodeIndexs[i][j];
+                    }
+
+                }
+                if(maxElement<pt+1){
+                    maxElement=pt+1;
+                    maxElementIndex=i;
+                }
+            }
+        }
+        Log.i(TAG,"maxElementIndex=="+maxElementIndex);
+
+
+        int charIndex=0;
+        int max,maxIndex;
+        while(ptArray[0]<decodeIndexs[0].length){
+            //Log.i(TAG,"ptArray[0]=="+ptArray[0]);
+            charIndex=decodeIndexs[maxElementIndex][ptArray[maxElementIndex]];//这里假设第一个数组包含所有的元素
+            for (i = 0; i < decodeIndexs.length; ++i) {
+
+                for (j = 0; j < sameCounter.length; ++j) {
+                    sameCounter[i] = 0;
+                }
+
+                if(decodeIndexs[i]!=null
+                    &&ptArray[i] < decodeIndexs[i].length
+                    &&decodeIndexs[i][ptArray[i]]==charIndex){
+                    sameCounter[i]=1;
+                    while (ptArray[i] < decodeIndexs[i].length - 1
+                        &&decodeIndexs[i][ptArray[i]] == decodeIndexs[i][ptArray[i] + 1]) {
+                        ptArray[i]++;
+                        sameCounter[i]++;
+                    }
+                    ptArray[i]++;
+                }
+            }
+            ///
+            for(i=0;i<sameCounter.length;++i){
+                Log.i(TAG,"sameCounter["+i+"]=="+sameCounter[i]);
+            }
+
+            for(i=0;i<sameNumberCounter.length;++i){
+                sameNumberCounter[i]=0;
+            }
+            for(int index:sameCounter){
+                sameNumberCounter[index]++;
+            }
+            ///
+            for(i=0;i<sameNumberCounter.length;++i){
+                Log.i(TAG,"sameNumberCounter["+i+"]=="+sameNumberCounter[i]);
+            }
+
+            max=sameNumberCounter[1];
+            maxIndex=1;
+            for(i=2;i<sameNumberCounter.length;++i){
+                if(max<sameNumberCounter[i]){
+                    max=sameNumberCounter[i];
+                    maxIndex=i;
+                }
+            }
+            Log.i(TAG,"maxIndex=="+maxIndex);
+
+            for(i=0;i<maxIndex;++i){
+                bestResult.add(charIndex);
+            }
+
+        }
+
+        return stringOfIndexs(toArray(bestResult));
     }
 
     /**
@@ -166,23 +284,23 @@ final class Decoder {
      * @return 返回一个数组，它包含了融合在一起的声音信息
      */
     private static int[] merge(int[] xArray,int[] yArray){
-        Log.i(TAG,"merge() : before merge : "+stringOfIndexs(xArray)+" ; "+stringOfIndexs(yArray)+" ;");
+        Log.i(TAG,"storeBestResult() : before storeBestResult : "+stringOfIndexs(xArray)+" ; "+stringOfIndexs(yArray)+" ;");
         //特殊情况的处理
         //1.
         if(xArray==null){
-            Log.i(TAG,"merge() : after merge : "+stringOfIndexs(yArray)+" ;");
+            Log.i(TAG,"storeBestResult() : after storeBestResult : "+stringOfIndexs(yArray)+" ;");
             return yArray;
         }
         else if(yArray==null){
-            Log.i(TAG,"merge() : after merge : "+stringOfIndexs(xArray)+" ;");
+            Log.i(TAG,"storeBestResult() : after storeBestResult : "+stringOfIndexs(xArray)+" ;");
             return xArray;
         }
         else if(yArray.length<2){
-            Log.i(TAG,"merge() : after merge : "+stringOfIndexs(xArray)+" ;");
+            Log.i(TAG,"storeBestResult() : after storeBestResult : "+stringOfIndexs(xArray)+" ;");
             return xArray;
         }
         else if(xArray.length<2){
-            Log.i(TAG,"merge() : after merge : "+stringOfIndexs(yArray)+" ;");
+            Log.i(TAG,"storeBestResult() : after storeBestResult : "+stringOfIndexs(yArray)+" ;");
             return yArray;
         }
         //2.如果两个参数相同
@@ -258,7 +376,7 @@ final class Decoder {
                 }
             }
         }
-        Log.i(TAG,"merge() : after merge : "+stringOfIndexs(toArray(resultArray))+" ;");
+        Log.i(TAG,"storeBestResult() : after storeBestResult : "+stringOfIndexs(toArray(resultArray))+" ;");
         return toArray(resultArray);*/
     }
 
