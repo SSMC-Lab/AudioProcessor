cd4e04e802f5a298bb10e1f927083dbded2b5404
GuoJieLiu <569746088@qq.com>
2016-12-05 16:27:12 +0800

    test_branch

--- app/src/main/java/fruitbasket/com/audioprocessor/ui/MainActivity.java
+++ app/src/main/java/fruitbasket/com/audioprocessor/ui/MainActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import fruitbasket.com.audioprocessor.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**/
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     private ViewPager mViewPager;
